53cf59c10b5dcd410ee472772819151431962d95
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-04-06 22:32:23 +0200

    Home page displayed

--- src/main/java/com/adminportal/controller/HomeController.java
+++ src/main/java/com/adminportal/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.adminportal.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// @RequestMapping("/adminportal")
 @Controller
 public class HomeController {
 
