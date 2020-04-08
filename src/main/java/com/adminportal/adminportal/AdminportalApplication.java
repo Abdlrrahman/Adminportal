153567b0318572a0ac21583eba7e8745ea57deab
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-04-08 19:14:49 +0200

    Added the login function

--- src/main/java/com/adminportal/adminportal/AdminportalApplication.java
+++ src/main/java/com/adminportal/adminportal/AdminportalApplication.java
@@ -1,5 +1,16 @@
 package com.adminportal.adminportal;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.adminportal.domain.User;
+import com.adminportal.domain.security.Role;
+import com.adminportal.domain.security.UserRole;
+import com.adminportal.service.UserService;
+import com.adminportal.utility.SecurityUtility;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,10 +22,28 @@ import org.springframework.context.annotation.ComponentScan;
 		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class })
 @ComponentScan("com.adminportal.controller")
 @EntityScan("com.adminportal.domain")
-public class AdminportalApplication {
+public class AdminportalApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserService userService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminportalApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		User user1 = new User();
+		user1.setUsername("admin");
+		user1.setPassword(SecurityUtility.passwordEncoder().encode("admin"));
+		user1.setEmail("admin@gmail.com");
+		Set<UserRole> userRoles = new HashSet<>();
+		Role role1 = new Role();
+		role1.setRoleId(1);
+		role1.setName("ROLE_ADMIN");
+		userRoles.add(new UserRole(user1, role1));
+
+		userService.createUser(user1, userRoles);
 	}
 
 }
---
+++ src/main/java/com/adminportal/service/UserService.java
@@ -0,0 +1,16 @@
+package com.adminportal.service;
+
+import java.util.Set;
+
+import com.adminportal.domain.User;
+import com.adminportal.domain.security.UserRole;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+
+    User createUser(User user, Set<UserRole> userRoles) throws Exception;
+
+    User save(User user);
+}
