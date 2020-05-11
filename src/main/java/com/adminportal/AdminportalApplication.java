e1785514beb6c643ac81068716794859cdcfe695
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-05-11 06:01:57 +0200

    config

--- src/main/java/com/adminportal/AdminportalApplication.java
+++ src/main/java/com/adminportal/AdminportalApplication.java
@@ -14,10 +14,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {
 		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class })
+@EnableJpaRepositories
 public class AdminportalApplication implements CommandLineRunner {
 
 	@Autowired
